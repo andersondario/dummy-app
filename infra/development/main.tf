dc1f375ec4034281a1578e9a72a066d769695b8b
andersondarioo <andersondario@gmail.com>
2022-07-19 17:20:44 -0300

    Refactor terraform definition and add new workflow

--- infra/development/main.tf
+++ infra/development/main.tf
@@ -1,12 +1,12 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "4.19.0"
-        }
-    }
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "4.19.0"
+		}
+	}
 
-    backend "remote" {
+	backend "remote" {
 		organization = "andersondario"
 		workspaces {
 			name = "dummy-app-development" 
@@ -15,14 +15,15 @@ terraform {
 }
 
 provider "aws" {
-    region     = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
+	region     = var.region
+	access_key = var.access_key
+	secret_key = var.secret_key
 }
 
 module "ecs" {
-    source       = "github.com/andersondario/terraform-sample//modules/aws/ecs"  
-    cluster_name = "cluster-qa"
-    app_name     = "dummy-app"
-    app_count    = 1
+	source       = "github.com/andersondario/terraform-modules//modules/aws/ecs"  
+	cluster_name = "cluster-qa"
+	app_name     = "dummy-app"
+	env_name     = "development"
+	app_count    = 1
 }
--- infra/production/main.tf
+++ infra/production/main.tf
@@ -1,12 +1,12 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "4.19.0"
-        }
-    }
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "4.19.0"
+		}
+	}
 
-    backend "remote" {
+	backend "remote" {
 		organization = "andersondario"
 		workspaces {
 			name = "dummy-app-production" 
@@ -15,14 +15,15 @@ terraform {
 }
 
 provider "aws" {
-    region     = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
+	region     = var.region
+	access_key = var.access_key
+	secret_key = var.secret_key
 }
 
 module "ecs" {
-    source       = "github.com/andersondario/terraform-sample//modules/aws/ecs"  
-    app_name     = "dummy-app"
-    cluster_name = "cluster-prod"
-    app_count    = 2
+	source       = "github.com/andersondario/terraform-modules//modules/aws/ecs"  
+	app_name     = "dummy-app"
+	cluster_name = "cluster-prod"
+	env_name     = "production"
+	app_count    = 2
 }
